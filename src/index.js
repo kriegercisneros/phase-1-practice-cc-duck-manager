90bb3037d4f31565a8fddcb10797ab8bfdc204a8
JackieCisneros <krieger.jacqueline@gmail.com>
2023-02-08 11:37:09 -0700

    finished mock cc single

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // write your code here!
+let currDuck; 
+
 fetch('http://localhost:3000/ducks')
 .then((resp) => resp.json())
 .then(data=>{
@@ -9,10 +11,13 @@ fetch('http://localhost:3000/ducks')
 const duckNav = document.querySelector('#duck-nav');
 //variables for displaying a single duck in the duck-display
 const duckDisplay = document.querySelector('#duck-display');
-const duckDisplayName = document.querySelector('#duck-display-name');
-const duckDisplayImage = document.querySelector('#duck-display-image');
+let duckDisplayName = document.querySelector('#duck-display-name');
+let duckDisplayImage = document.querySelector('#duck-display-image');
 //variables for accessing like button 
 let duckDisplayLikes = document.querySelector('#duck-display-likes');
+//variables for accessing newDuckForm 
+const newDuckForm = document.querySelector('#new-duck-form');
+
 
 
 
@@ -31,13 +36,47 @@ function populateDucks(ducks){
 }
 
 function displaySingleDuck(singleDuck){
-    duckDisplayName.innerText = singleDuck.name;
-    duckDisplayImage.src = singleDuck.img_url;
-    duckDisplayLikes.innerText = singleDuck.likes;
+    currDuck = singleDuck
+    
+    duckDisplayName.innerText = currDuck.name;
+    duckDisplayImage.src = currDuck.img_url;
+    duckDisplayLikes.innerText = currDuck.likes;
 }
 
 duckDisplayLikes.addEventListener('click', ()=>{
     //when user clicks, increments the ducksDisplayLikes by 1
-    duckDisplayLikes.innerText++
+    let currLikes = duckDisplayLikes.innerText++
+    currDuck.likes = currLikes
+    // displaySingleDuck(currDuck)
 })
+
+newDuckForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    debugger
+    const newDuck = {
+        name : e.target['duck-name-input'].value,
+        img_url : e.target['duck-image-input'].value,
+        likes :  0
+    }
+    function appendNewDuck(ele){
+        const img = document.createElement('img');
+        img.src = ele.img_url;
+
+        duckNav.append(img);
+
+        img.addEventListener('click', () => {
+            displaySingleDuck(ele)
+        })
+    }
+    appendNewDuck(newDuck)
+    // duckNav.append(newDuck);
+    // displaySingleDuck(newDuck)
+
+    // duckNav.append(ele.img_url);
+        // ele.addEventListener('click', () => {
+        //     displaySingleDuck(ele)
+        // })
+})
+
+
 
